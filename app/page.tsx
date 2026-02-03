a60cb804f257dc59d4f61403cc8e3c4f1e8f01f5
Viktoriia Yevpak <viktoriia.yevpak@cosmonova-broadcast.tv>
2026-02-03 13:04:53 +0200

    Add `Hero` component with GSAP animations and integrate it into `page.tsx`

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+'use client'
+
 import gsap from 'gsap'
 import { ScrollTrigger, SplitText } from 'gsap/all'
 import Navbar from '@/components/Navbar'
+import Hero from '@/components/Hero'
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
@@ -8,6 +11,8 @@ export default function Home() {
   return (
     <main>
       <Navbar />
+      <Hero />
+      <div className='h-dvh bg-black'></div>
     </main>
   )
 }
